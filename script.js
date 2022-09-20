0dcac2d4c17eb517cdfc54f328b10093e09d237f
Yana <norinskaya_yana@mail.ru>
2022-09-20 18:06:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelectorAll('input'),
+    result = document.querySelector('#result');
+
+let resCalc;
+input.forEach(item => {
+    item.addEventListener('click', e => {
+        if (e.target.value == 'C') {
+            result.value = '';
+        } else if (e.target.value == '=') {
+            if (result.value == '') {
+                result.value = '';
+            } else { result.value = eval(result.value); }
+
+        } else {
+            result.value += e.target.value;
+        }
+
+    })
+})
+
+
+
